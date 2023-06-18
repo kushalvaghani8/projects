1f58d92e243398f5b82dd84e497292ff31026259
kushalvaghani8 <97976457+kushalvaghani8@users.noreply.github.com>
2023-06-18 18:59:14 +0000

    commit before installing gunicorn

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask('app')
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  return render_template("index.html")
+
+@app.route('/python')
+def python():
+  return render_template("python.html")
+
+@app.route('/ios')
+def ios():
+  return render_template("ios.html")
+
+@app.route('/sql')
+def sql():
+  return render_template("sql.html") 
+
+@app.route('/android')
+def android():
+  return render_template("android.html")
 
 app.run(host='0.0.0.0', port=8080)
