d1331ca97839114a45af1f06ef749033305e0d7f
kushalvaghani <A00247163@mycambrian.ca>
2023-07-17 15:19:09 -0400

    checking

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
 from flask import Flask, render_template
-myflaskapp = Flask(__name__)
+app = Flask(__name__)
 
-@myflaskapp.route('/')
+@app.route('/')
 def hello_world():
   return render_template("index.html")
 
-@myflaskapp.route('/python')
+@app.route('/python')
 def python():
   return render_template("python.html")
 
-@myflaskapp.route('/ios')
+@app.route('/ios')
 def ios():
   return render_template("ios.html")
 
-@myflaskapp.route('/resume')
+@app.route('/resume')
 def resume():
   return render_template("resume.html") 
 
-@myflaskapp.route('/android')
+@app.route('/android')
 def android():
   return render_template("android.html")
 
-if __name__ == "__main__":
-    myflaskapp.run(host='0.0.0.0', port=8080)
+if __name__ == "__app__":
+    app.run(host='0.0.0.0', port=8080)
